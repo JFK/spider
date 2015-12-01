d240e6354419bf90216f0c5f5b95b915abf9a94f
Fumikazu Kiyota <fumikazu.kiyota@vuzz.com>
2015-12-01 15:11:39 +0900

    Update proxy.py

--- www/proxy.py
+++ www/proxy.py
@@ -6,19 +6,25 @@ from time import sleep
 from pymongo import MongoClient
 import importlib
 import sys
-import optparse
+import argparse
 import configparser
+from datetime import datetime
+from ast import literal_eval
+import re
+
+TAG_NAME_PREFIX = 'pub-proxy'
 
 
-def update_worker(db, pname, proxy):
-    mongo = MongoClient(db['HOST'], db['PORT'])
-    db = mongo[db['DB']]
-    cond = {'pname': pname}
-    sets = {'$set': {'proxy': proxy}}
-    getattr(db, 'worker').update(cond, sets)
+def db_update_proxy(db, proxy):
+    cond = {'http': proxy['http']}
+    sets = {'$set': proxy}
+    getattr(db, 'proxy').update(cond, sets, upsert=True)
+    for x in getattr(db, 'proxy').find({}):
+        print x['http']
 
 
-def add_proxy(ec2):
+def add_proxy(ec2, tag_name, db):
+    print 'adding proxy... %s' % tag_name
     req = ec2.request_spot_instances(price=price,
                                      instance_type=instance_type,
                                      image_id=image_id,
@@ -27,6 +33,8 @@ def add_proxy(ec2):
                                      security_group_ids=security_group_ids)
     job_instance_id = None
     job_sir_id = None
+    sys.stdout.write('Please wait')
+    sys.stdout.flush()
     while not job_instance_id:
         job_sir_id = req[0].id
         reqs = ec2.get_all_spot_instance_requests()
@@ -39,63 +47,118 @@ def add_proxy(ec2):
             sleep(1)
     ec2.create_tags([job_instance_id], {'Name': tag_name})
     ec2.cancel_spot_instance_requests([job_sir_id])
+    print 'done!'
+    update_proxy(ec2, db)
 
 
-def update_proxy(ec2, pname, db):
+def list_proxy(ec2, db):
     all_instances = ec2.get_all_instances()
-    proxies = []
+    instance_ids = []
+    for inst in all_instances:
+        for x in inst.instances:
+            if re.match(TAG_NAME_PREFIX, x.tags.get('Name')):
+                if 'running' in str(x._state):
+                    instance_ids.append(x.id)
+                    print x.tags.get('Name'), x.id, x._state
+    if not instance_ids:
+        print 'not found...'
+    else:
+        update_proxy(ec2, db)
+
+
+def delete_proxy(ec2, tag_name, db):
+    print 'deleting proxy... %s' % tag_name
+    all_instances = ec2.get_all_instances()
+    instance_ids = []
     for inst in all_instances:
         for x in inst.instances:
             if x.tags.get('Name') == tag_name:
                 if 'running' in str(x._state):
+                    instance_ids.append(x.id)
+                    print tag_name, x.id, x._state
+    ec2.terminate_instances(instance_ids)
+    update_proxy(ec2, db)
+
+
+def update_proxy(ec2, db):
+    print 'updating proxy data...'
+    all_instances = ec2.get_all_instances()
+    for inst in all_instances:
+        for x in inst.instances:
+            if re.match(TAG_NAME_PREFIX, x.tags.get('Name')):
+                if 'running' in str(x._state):
                     url = x.public_dns_name
                     print x.id, x._state, url
                     proxy = {
-                        'http:': 'http://%s:8080' % url
+                        'http': 'http://%s:8080' % url
                     }
-                    proxies.append(proxy)
-    update_worker(db, pname, proxies)
+                    db_update_proxy(db, proxy)
 
-
-def main(pname, db, add_proxy=False):
-    ec2 = boto_ec2.connect_to_region(region_name=region_name,
-                                     aws_access_key_id=aws_access_key_id,
-                                     aws_secret_access_key=aws_secret_access_key)
-    if add_proxy:
-        add_proxy(ec2)
-    update_proxy(ec2, pname, db)
 
 if __name__ == '__main__':
-    parser = optparse.OptionParser()
-    parser.add_option('-c', '--config',
-                      action="store", dest="c",
-                      default='.aws/config',
-                      help="aws config")
-    parser.add_option('-p', '--project-name',
-                      action="store", dest="p",
-                      help="project module name")
-    parser.add_option('-a', '--add-proxy', action="store_true",
-                      dest="add", default=False,
-                      help="add new proxy")
-    opts, args = parser.parse_args()
-    config = configparser.ConfigParser()
-    config.read(opts.c)
     try:
+        parser = argparse.ArgumentParser(description='Spider Proxy Tools')
+        parser.add_argument('-c', '--config',
+                            action="store", dest="c",
+                            default='.aws/config',
+                            required=False,
+                            help="aws config")
+        parser.add_argument('-a', '--add-proxy', action="store_true",
+                            dest="add_proxy", default=False,
+                            help="add new proxy")
+        parser.add_argument('--delete-proxy', action="store",
+                            default=None, required=False,
+                            help="proxy tag name")
+        parser.add_argument('-H', '--host', action="store", dest="h",
+                            default='localhost', required=False,
+                            help="mongodb host")
+        parser.add_argument('-p', '--port', action="store", dest="p",
+                            default=27017, required=False,
+                            help="mongodb port")
+        parser.add_argument('-d', '--db', action="store", dest="db",
+                            default='proxydb', required=False,
+                            help="mongodb db")
+        args = parser.parse_args()
+        config = configparser.ConfigParser()
+        config.read(args.c)
         aws_access_key_id = config.get('default', 'aws_access_key_id')
         aws_secret_access_key = config.get('default', 'aws_secret_access_key')
         region_name = config.get('default', 'region_name')
         price = config.get('default', 'price')
         instance_type = config.get('default', 'instance_type')
         image_id = config.get('default', 'image_id')
-        availability_zone_group = config.get('default', 'availability_zone_group')
         key_name = config.get('default', 'key_name')
-        security_group_ids = config.get('default', 'security_group_ids')
-        tag_name = config.get('default', 'tag_name')
+        availability_zone_group = literal_eval(
+            config.get('default', 'availability_zone_group')
+        )
+        security_group_ids = literal_eval(
+            config.get('default', 'security_group_ids')
+        )
+        tag_name = '%s-%s' % (TAG_NAME_PREFIX,
+                              datetime.utcnow().strftime('%Y%m%d%H%I%S'))
+        db = {
+            'HOST': args.h,
+            'PORT': int(args.p),
+            'DB': args.db
+        }
+        mongo = MongoClient(args.h, int(args.p))
+        db = mongo[args.db]
+        ec2 = boto_ec2.connect_to_region(region_name=region_name,
+                                         aws_access_key_id=aws_access_key_id,
+                                         aws_secret_access_key=aws_secret_access_key)
+        if args.delete_proxy:
+            delete_proxy(ec2, args.delete_proxy, db)
+
+        elif args.add_proxy:
+            add_proxy(ec2, tag_name, db)
+
+        else:
+            list_proxy(ec2, db)
+
     except:
         print 'Please create .aws/config and add these configs'
         print '$ mkdir .aws'
         print '$ cat <<EOF > .aws/config'
-        print '[default]'
         print 'aws_access_key_id = <aws_access_key_id>'
         print 'aws_secret_access_key = <aws_secret_access_key>'
         print 'region_name = ap-northeast-1'
@@ -105,10 +168,5 @@ if __name__ == '__main__':
         print 'availability_zone_group = ["ap-northeast-1b"]'
         print 'key_name = proxy'
         print 'security_group_ids = ["sg-22d52b22"]'
-        print 'tag_name = pub-proxy-0'
         print 'EOF'
-        sys.exit(1)
-
-    m = importlib.import_module('projects.%s' % opts.p)
-    db = getattr(m, 'MONGODB')
-    main(opts.p, db, add_proxy=opts.add)
+        importlib.import_module('mylib.logger').sentry()
