49a8d02b1d6dd1bb37f921ecd7f2fb7551f747d6
Fumikazu Kiyota <fumikazu.kiyota@vuzz.com>
2015-12-01 22:45:48 +0900

    Update proxy.py

--- www/proxy.py
+++ www/proxy.py
@@ -10,12 +10,38 @@ import argparse
 import configparser
 from datetime import datetime
 from ast import literal_eval
+import random
 import re
 
 TAG_NAME_PREFIX = 'pub-proxy'
+mongo = MongoClient()
+db = mongo['proxydb']
+
+config = configparser.ConfigParser()
+config.read('.aws/config')
+aws_access_key_id = config.get('default', 'aws_access_key_id')
+aws_secret_access_key = config.get('default', 'aws_secret_access_key')
+region_name = config.get('default', 'region_name')
+price = config.get('default', 'price')
+instance_type = config.get('default', 'instance_type')
+image_id = config.get('default', 'image_id')
+key_name = config.get('default', 'key_name')
+availability_zone_group = literal_eval(
+    config.get('default', 'availability_zone_group')
+)
+security_group_ids = literal_eval(
+    config.get('default', 'security_group_ids')
+)
+ec2 = boto_ec2.connect_to_region(region_name=region_name,
+                                 aws_access_key_id=aws_access_key_id,
+                                 aws_secret_access_key=aws_secret_access_key)
 
 
-def db_update_proxy(db, proxy):
+def select_proxy():
+    return random.choice(map(lambda doc: doc, db.proxy.find({})))
+
+
+def db_update_proxy(proxy):
     cond = {'http': proxy['http']}
     sets = {'$set': proxy}
     getattr(db, 'proxy').update(cond, sets, upsert=True)
@@ -23,7 +49,8 @@ def db_update_proxy(db, proxy):
         print x['http']
 
 
-def add_proxy(ec2, tag_name, db):
+def add_proxy():
+    tag_name = create_tag_name()
     print 'adding proxy... %s' % tag_name
     req = ec2.request_spot_instances(price=price,
                                      instance_type=instance_type,
@@ -33,7 +60,7 @@ def add_proxy(ec2, tag_name, db):
                                      security_group_ids=security_group_ids)
     job_instance_id = None
     job_sir_id = None
-    sys.stdout.write('Please wait')
+    sys.stdout.write('please wait')
     sys.stdout.flush()
     while not job_instance_id:
         job_sir_id = req[0].id
@@ -48,10 +75,10 @@ def add_proxy(ec2, tag_name, db):
     ec2.create_tags([job_instance_id], {'Name': tag_name})
     ec2.cancel_spot_instance_requests([job_sir_id])
     print 'done!'
-    update_proxy(ec2, db)
+    update_proxy()
 
 
-def list_proxy(ec2, db):
+def list_proxy():
     all_instances = ec2.get_all_instances()
     instance_ids = []
     for inst in all_instances:
@@ -63,10 +90,52 @@ def list_proxy(ec2, db):
     if not instance_ids:
         print 'not found...'
     else:
-        update_proxy(ec2, db)
+        update_proxy()
 
 
-def delete_proxy(ec2, tag_name, db):
+def create_tag_name():
+    return '%s-%s' % (TAG_NAME_PREFIX,
+                      datetime.utcnow().strftime('%Y%m%d%H%I%S'))
+
+
+def shutdown_proxy():
+    print 'shutting down proxy...'
+    all_instances = ec2.get_all_instances()
+    instance_ids = []
+    for inst in all_instances:
+        for x in inst.instances:
+            if re.match(TAG_NAME_PREFIX, x.tags.get('Name')):
+                if 'running' in str(x._state):
+                    instance_ids.append(x.id)
+    if not instance_ids:
+        print 'not found any proxy...'
+    else:
+        print 'terminating instances... %s' % ", ".join(instance_ids)
+        ec2.terminate_instances(instance_ids)
+        getattr(db, 'proxy').remove({})
+
+
+def reload_proxy():
+    print 'reloadng proxy...'
+    all_instances = ec2.get_all_instances()
+    instance_ids = []
+    for inst in all_instances:
+        for x in inst.instances:
+            if re.match(TAG_NAME_PREFIX, x.tags.get('Name')):
+                if 'running' in str(x._state):
+                    instance_ids.append(x.id)
+    if not instance_ids:
+        print 'not found any proxy...'
+    else:
+        for x in range(0, len(instance_ids)):
+            add_proxy()
+        print 'terminating instances... %s' % ", ".join(instance_ids)
+        ec2.terminate_instances(instance_ids)
+        getattr(db, 'proxy').remove({})
+        update_proxy()
+
+
+def delete_proxy(tag_name):
     print 'deleting proxy... %s' % tag_name
     all_instances = ec2.get_all_instances()
     instance_ids = []
@@ -76,11 +145,15 @@ def delete_proxy(ec2, tag_name, db):
                 if 'running' in str(x._state):
                     instance_ids.append(x.id)
                     print tag_name, x.id, x._state
-    ec2.terminate_instances(instance_ids)
-    update_proxy(ec2, db)
+    if not instance_ids:
+        print 'not found any proxy...'
+    else:
+        ec2.terminate_instances(instance_ids)
+        getattr(db, 'proxy').remove({})
+        update_proxy()
 
 
-def update_proxy(ec2, db):
+def update_proxy():
     print 'updating proxy data...'
     all_instances = ec2.get_all_instances()
     for inst in all_instances:
@@ -92,68 +165,39 @@ def update_proxy(ec2, db):
                     proxy = {
                         'http': 'http://%s:8080' % url
                     }
-                    db_update_proxy(db, proxy)
+                    db_update_proxy(proxy)
 
 
 if __name__ == '__main__':
     try:
         parser = argparse.ArgumentParser(description='Spider Proxy Tools')
-        parser.add_argument('-c', '--config',
-                            action="store", dest="c",
-                            default='.aws/config',
-                            required=False,
-                            help="aws config")
-        parser.add_argument('-a', '--add-proxy', action="store_true",
-                            dest="add_proxy", default=False,
+        parser.add_argument('--reload', action="store_true",
+                            dest="reload", default=False,
+                            help="reload proxy")
+        parser.add_argument('--add', action="store_true",
+                            dest="add", default=False,
                             help="add new proxy")
-        parser.add_argument('--delete-proxy', action="store",
+        parser.add_argument('--shutdown', action="store_true",
+                            dest="shutdown", default=False,
+                            help="shutdown all proxies")
+        parser.add_argument('--delete', action="store",
                             default=None, required=False,
-                            help="proxy tag name")
-        parser.add_argument('-H', '--host', action="store", dest="h",
-                            default='localhost', required=False,
-                            help="mongodb host")
-        parser.add_argument('-p', '--port', action="store", dest="p",
-                            default=27017, required=False,
-                            help="mongodb port")
-        parser.add_argument('-d', '--db', action="store", dest="db",
-                            default='proxydb', required=False,
-                            help="mongodb db")
+                            help="delete proxy")
         args = parser.parse_args()
-        config = configparser.ConfigParser()
-        config.read(args.c)
-        aws_access_key_id = config.get('default', 'aws_access_key_id')
-        aws_secret_access_key = config.get('default', 'aws_secret_access_key')
-        region_name = config.get('default', 'region_name')
-        price = config.get('default', 'price')
-        instance_type = config.get('default', 'instance_type')
-        image_id = config.get('default', 'image_id')
-        key_name = config.get('default', 'key_name')
-        availability_zone_group = literal_eval(
-            config.get('default', 'availability_zone_group')
-        )
-        security_group_ids = literal_eval(
-            config.get('default', 'security_group_ids')
-        )
-        tag_name = '%s-%s' % (TAG_NAME_PREFIX,
-                              datetime.utcnow().strftime('%Y%m%d%H%I%S'))
-        db = {
-            'HOST': args.h,
-            'PORT': int(args.p),
-            'DB': args.db
-        }
-        mongo = MongoClient(args.h, int(args.p))
-        db = mongo[args.db]
-        ec2 = boto_ec2.connect_to_region(region_name=region_name,
-                                         aws_access_key_id=aws_access_key_id,
-                                         aws_secret_access_key=aws_secret_access_key)
-        if args.delete_proxy:
-            delete_proxy(ec2, args.delete_proxy, db)
+        if args.shutdown:
+            shutdown_proxy()
 
-        elif args.add_proxy:
-            add_proxy(ec2, tag_name, db)
+        elif args.reload:
+            reload_proxy()
+
+        elif args.delete:
+            delete_proxy(args.delete)
+
+        elif args.add:
+            add_proxy()
 
         else:
-            list_proxy(ec2, db)
+            list_proxy()
 
     except:
         print 'Please create .aws/config and add these configs'
