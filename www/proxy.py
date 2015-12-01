0a2a92a3a0b0db90358efd3d355f13b29fe47ef2
jfk <fumikazu.kiyota@gmail.com>
2015-12-01 12:21:38 +0900

    Update config for aws

--- www/proxy.py
+++ www/proxy.py
@@ -7,23 +7,47 @@ from pymongo import MongoClient
 import importlib
 import sys
 import optparse
-try:
-    from mylib import aws
-except:
-    print 'Please create mylib/aws.py'
-    print 'e.g'
-    print 'aws_access_key_id = "<aws_access_key_id>"'
-    print 'aws_secret_access_key = "<aws_secret_access_key>"'
-    print 'region_name = "ap-northeast-1"'
-    print 'price = 0.5'
-    print 'image_id = "ami-13527d7d"'
-    print 'instance_type = "m3.medium"'
-    print 'availability_zone_group = ["ap-northeast-1b"]'
-    print 'key_name = "proxy"'
-    print 'security_group_ids = ["sg-22d52b22"]'
-    print 'tag_name = "pub-proxy"'
+import configparser
 
-print 'Creating spot request...'
+
+config = configparser.ConfigParser()
+config.read('.aws/config')
+
+az = {
+    'b': ['ap-northeast-1b'],
+    'c': ['ap-northeast-1c']
+}
+subnet = {
+    'b': 'subnet-4654242f',
+    'c': 'subnet-11532378'
+}
+try:
+    aws_access_key_id = config.get('default', 'aws_access_key_id')
+    aws_secret_access_key = config.get('default', 'aws_secret_access_key')
+    region_name = config.get('default', 'region_name')
+    price = config.get('default', 'price')
+    instance_type = config.get('default', 'instance_type')
+    image_id = config.get('default', 'image_id')
+    availability_zone_group = config.get('default', 'availability_zone_group')
+    key_name = config.get('default', 'key_name')
+    security_group_ids = config.get('default', 'security_group_ids')
+    tag_name = config.get('default', 'tag_name')
+except:
+    print 'Please create .aws/config and add these configs'
+    print '$ mkdir .aws'
+    print '$ cat <<EOF > .aws/config'
+    print 'aws_access_key_id = <aws_access_key_id>'
+    print 'aws_secret_access_key = <aws_secret_access_key>'
+    print 'region_name = ap-northeast-1'
+    print 'price = 0.5'
+    print 'image_id = ami-13527d7d'
+    print 'instance_type = m3.medium'
+    print 'availability_zone_group = ["ap-northeast-1b"]'
+    print 'key_name = proxy'
+    print 'security_group_ids = ["sg-22d52b22"]'
+    print 'tag_name = pub-proxy-0'
+    print 'EOF'
+    sys.exit(1)
 
 
 def update_worker(db, pname, proxy):
@@ -34,13 +58,13 @@ def update_worker(db, pname, proxy):
     getattr(db, 'worker').update(cond, sets)
 
 
-def checking(ec2):
-    req = ec2.request_spot_instances(price=aws.price,
-                                     instance_type=aws.instance_type,
-                                     image_id=aws.image_id,
-                                     availability_zone_group=aws.availability_zone_group,
-                                     key_name=aws.key_name,
-                                     security_group_ids=aws.security_group_ids)
+def add_proxy(ec2):
+    req = ec2.request_spot_instances(price=price,
+                                     instance_type=instance_type,
+                                     image_id=image_id,
+                                     availability_zone_group=availability_zone_group,
+                                     key_name=key_name,
+                                     security_group_ids=security_group_ids)
     job_instance_id = None
     job_sir_id = None
     while not job_instance_id:
@@ -53,7 +77,7 @@ def checking(ec2):
             sys.stdout.write('.')
             sys.stdout.flush()
             sleep(1)
-    ec2.create_tags([job_instance_id], {'Name': aws.tag_name})
+    ec2.create_tags([job_instance_id], {'Name': tag_name})
     ec2.cancel_spot_instance_requests([job_sir_id])
 
 
@@ -62,7 +86,7 @@ def update_proxy(ec2, pname, db):
     proxies = []
     for inst in all_instances:
         for x in inst.instances:
-            if x.tags.get('Name') == aws.tag_name:
+            if x.tags.get('Name') == tag_name:
                 if 'running' in str(x._state):
                     url = x.public_dns_name
                     print x.id, x._state, url
@@ -74,11 +98,11 @@ def update_proxy(ec2, pname, db):
 
 
 def main(pname, db, add_proxy=False):
-    ec2 = boto_ec2.connect_to_region(region_name=aws.region_name,
-                                     aws_access_key_id=aws.aws_access_key_id,
-                                     aws_secret_access_key=aws.aws_secret_access_key)
+    ec2 = boto_ec2.connect_to_region(region_name=region_name,
+                                     aws_access_key_id=aws_access_key_id,
+                                     aws_secret_access_key=aws_secret_access_key)
     if add_proxy:
-        checking(ec2)
+        add_proxy(ec2)
     update_proxy(ec2, pname, db)
 
 if __name__ == '__main__':
